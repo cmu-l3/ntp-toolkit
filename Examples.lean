aa1c4a77be84a92178311eef10aa053f8d0323cd
JOSHCLUNE <josh.seth.clune@gmail.com>
2024-08-18 08:29:38 -0400

    Added tactic_benchmark.lean

--- Examples.lean
+++ Examples.lean
@@ -1,2 +1,3 @@
 
 import Mathlib
+import temp
--- temp.lean
+++ temp.lean
@@ -5,10 +5,11 @@ axiom q_eq_true : q = True
 
 -- The `goalDelta` approach fails because `have : p = True := by exact p_eq_true` does
 -- not open a goal but `by exact p_eq_true` closes a goal
-theorem list_eq_self : ∀ l : List α, l = l := by
-  intro l
+theorem list_eq_self (l : List α) : l = l := by
   cases l
   . have : p = True := by exact p_eq_true
     rfl
   . have : q = True := by exact q_eq_true
     rfl
+
+theorem zero_eq_zero : 0 = 0 := by omega
