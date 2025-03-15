9c90269de18f7f2c715965ca688c4630dd5bf7e1
JOSHCLUNE <josh.seth.clune@gmail.com>
2025-03-15 15:28:45 -0400

    Add dependency to Bartosz's KNN and Forest premise selection

--- lakefile.lean
+++ lakefile.lean
@@ -11,6 +11,9 @@ require mathlib from git
 require QuerySMT from git
   "https://github.com/JOSHCLUNE/LeanSMTParser.git" @ "842ca2a4a780f1a3589c930ef82ebb08368682f7"
 
+require leanPremiseSelection from git
+  "https://github.com/JOSHCLUNE/lean-premise-selection.git" @ "25909ec183974f5a52f9e733475cd635d5c8ead7"
+
 require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "v4.16.0"
 
 @[default_target]
