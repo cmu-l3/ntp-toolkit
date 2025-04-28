b550d52b87d9aef42cfcbcaf57ff51f81055b80b
JOSHCLUNE <josh.seth.clune@gmail.com>
2025-04-27 21:03:24 -0400

    Update querySMT

--- scripts/extract_repos.py
+++ scripts/extract_repos.py
@@ -17,7 +17,7 @@ require leanPremiseSelection from git
   "https://github.com/JOSHCLUNE/lean-premise-selection.git" @ "86f02182e5b30737b41aae20b8ef59d3f03d0a84"
 
 require QuerySMT from git
-  "https://github.com/JOSHCLUNE/LeanSMTParser.git" @ "87c56a696de111d3dced49e9413b6f03bde0e8c7"
+  "https://github.com/JOSHCLUNE/LeanSMTParser.git" @ "15c641b2f5330aef1451e97d1c5fcf7ad584ffcf"
 
 require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "v4.18.0"
 
