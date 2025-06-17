9aad70f1cf659ad8bc7568bb76cb2ba904b933eb
Thomas Zhu <thomas.zhu.sh@hotmail.com>
2025-06-17 09:37:34 -0700

    remove --skip_setup in the first setup

--- extract.sh
+++ extract.sh
@@ -13,7 +13,7 @@ conda activate lm
 MAX_WORKERS=16 # set according to your RAM capacity
 CONFIG=configs/config_mathlib_full.json
 rm -rf Examples/mathlib
-python scripts/extract_repos.py --config $CONFIG --cwd "`pwd`" --imports --skip_setup --max-workers $MAX_WORKERS
+python scripts/extract_repos.py --config $CONFIG --cwd "`pwd`" --imports --max-workers $MAX_WORKERS
 python scripts/extract_repos.py --config $CONFIG --cwd "`pwd`" --declarations --skip_setup --max-workers $MAX_WORKERS
 python scripts/extract_repos.py --config $CONFIG --cwd "`pwd`" --training_data_with_premises --skip_setup --max-workers $MAX_WORKERS
 python scripts/extract_repos.py --config $CONFIG --cwd "`pwd`" --full_proof_training_data --skip_setup --max-workers $MAX_WORKERS  # this one is not necessary; only used for e.g. proof length analysis in paper
